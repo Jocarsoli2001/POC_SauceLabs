95738d6c0a6d6a230406d15c62614cf7b742fb69
Jocarsoli2001 <Jcsantizo@tigo.com.gt>
2025-05-27 16:50:36 -0600

    Prueba 2: Implementación de script en dispositivos IOS

--- test_saucelabs/src/test/java/com/test/poc/AppTest.java
+++ test_saucelabs/src/test/java/com/test/poc/AppTest.java
@@ -10,11 +10,13 @@ import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 
 public class AppTest {
 
     // Declare a field to store the AndroidDriver instance
     private AndroidDriver driver;
+    private IOSDriver iosDriver;
 
     @Test
     public void setUp() throws MalformedURLException {
@@ -22,18 +24,22 @@ public class AppTest {
         MutableCapabilities capabilities = new MutableCapabilities();
 
         capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("platformName", "android");
-        capabilities.setCapability("appium:app", "storage:filename=Android-MyDemoAppRN.1.3.0.build-244.apk"); // The filename of the mobile app
+        // capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("platformName", "ios");
+        // capabilities.setCapability("appium:app", "storage:filename=Android-MyDemoAppRN.1.3.0.build-244.apk"); // The filename of the mobile app
+        capabilities.setCapability("appium:app", "storage:filename=iOS-Real-Device-MyRNDemoApp.1.3.0-162.ipa");
         // W3C Protocol is mandatory for Appium 2
         capabilities.setCapability("appium:platformVersion", "12");
-        capabilities.setCapability("appium:deviceName", "Google_Pixel_5_free");
+        // capabilities.setCapability("appium:deviceName", "Samsung_Galaxy_S23_15_real_sjc1");
+        capabilities.setCapability("appium:deviceName", "iPhone_11_18_real_sjc1");
         // Mandatory for Appium 2
-        capabilities.setCapability("appium:automationName", "uiautomator2");
+        // capabilities.setCapability("appium:automationName", "uiautomator2");
+        capabilities.setCapability("appium:automationName", "xcuitest");
 
         HashMap<String, Object> sauceOptions = new HashMap<String, Object>();
         // appiumVersion is mandatory to use Appium 2 on Sauce Labs
         sauceOptions.put("appiumVersion", "2.0.0");
-        sauceOptions.put("build", "appium-build-60B44");
+        sauceOptions.put("build", "162");
         sauceOptions.put("name", "Sauce Labs Appium Test");
         sauceOptions.put("username", "Jocarsoli2001");
         sauceOptions.put("accessKey", "71f6843b-dfff-4220-aa49-e64c81034606");
@@ -46,7 +52,8 @@ public class AppTest {
 
         // Start the session
         URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
-        AndroidDriver driver = new AndroidDriver(url, capabilities);
+        // driver = new AndroidDriver(url, capabilities);
+        IOSDriver iosDriver = new IOSDriver(url, capabilities);
 
         // replace with commands and assertions
         try {
@@ -55,15 +62,32 @@ public class AppTest {
             e.printStackTrace();
         }
         
+        // try {
+        //     WebElement element = driver.findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"store item\"])[1]/android.view.ViewGroup[1]/android.widget.ImageView"));
+        //     element.click();
+        //     WebElement element2 = driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"open menu\"]/android.widget.ImageView"));
+        //     element2.click();
+        //     driver.executeScript("sauce:job-result=passed");
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        //     driver.executeScript("sauce:job-result=failed");
+        // }
+
         try {
-            WebElement element = driver.findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"store item\"])[1]/android.view.ViewGroup[1]/android.widget.ImageView"));
+            WebElement element = iosDriver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"Sauce Labs Backpack\"]"));
             element.click();
-            WebElement element2 = driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"open menu\"]/android.widget.ImageView"));
+            WebElement element2 = iosDriver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"red circle\"]"));
             element2.click();
-            driver.executeScript("sauce:job-result=passed");
+            WebElement element3 = iosDriver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"counter plus button\"]"));
+            element3.click();
+            element3.click();
+            element3.click();
+            WebElement element4 = iosDriver.findElement(By.xpath("//XCUIElementTypeOther[@name=\"navigation back button\"]"));
+            element4.click();
+            iosDriver.executeScript("sauce:job-result=passed");
         } catch (Exception e) {
             e.printStackTrace();
-            driver.executeScript("sauce:job-result=failed");
+            iosDriver.executeScript("sauce:job-result=failed");
         }
         
         try {
@@ -72,7 +96,7 @@ public class AppTest {
             e.printStackTrace();
         }
 
-        driver.quit();
+        iosDriver.quit();
     }
 
 }
